efd9f5e14cd2bcc4166f4a12be5a0c4457d7242c
maheeshpm7 <104315264+maheeshpm7@users.noreply.github.com>
2026-06-04 17:42:39 +0530

    Change desired_size from 1 to 3 in EKS config

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -90,7 +90,7 @@ module "eks" {
     tws-demo-ng = {
       min_size     = 1
       max_size     = 3
-      desired_size = 1
+      desired_size = 3
 
       instance_types = ["t3.large"]
       capacity_type  = "SPOT"
